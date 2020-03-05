0d9cc479aecbc18fa61ba2fc1b2499e55226ee30
cleavengyj <60376701+cleavengyj@users.noreply.github.com>
2020-03-05 17:50:52 +0800

    working but subject to changes
    working A*, not optimized, might change to use heapq/ priority queues, but in general the idea is here

--- rawLogic.py
+++ rawLogic.py
@@ -3,93 +3,91 @@ import networkx as nx
 
 G = nx.Graph()
 
-'''
-for nodes within area of zoom (to be specified) depending on startNode and endNode:
-  G.add_edge(nodeId, nextnodeId, weight = distance by road)
-
-#for here i'll be using fake nodes
-'''
-
 G.add_edge('a', 'b', weight= 60) #weight = road dist correct to 2dp.
-G.add_edge('a', 'c', weight = 27)
+G.add_edge('a', 'c', weight= 17)
 G.add_edge('c', 'd', weight= 10)
 G.add_edge('c', 'e', weight= 70)
 G.add_edge('c', 'f', weight= 90)
 G.add_edge('a', 'd', weight= 30)
 G.add_edge('d', 'e', weight= 30)
 
-G.add_node('a', gVal=0, hVal=0, fVal=20)
-G.add_node('b', gVal=0, hVal=0, fVal=15)
-G.add_node('c', gVal=0, hVal=0, fVal=21)
-G.add_node('d', gVal=0, hVal=0, fVal=16)
-G.add_node('e', gVal=0, hVal=0, fVal=32)
-G.add_node('f', gVal=0, hVal=0, fVal=28)
+G.add_node('a', gVal=0, hVal=30, fVal=0)
+G.add_node('b', gVal=0, hVal=100, fVal=0)
+G.add_node('c', gVal=0, hVal=50, fVal=0)
+G.add_node('d', gVal=0, hVal=20, fVal=0)
+G.add_node('e', gVal=0, hVal=0, fVal=0)
+G.add_node('f', gVal=0, hVal=120, fVal=0)
 
-# function to find hVal (direct dist)
-# function to find gVal (collective dist)
-# then fVal = gVal + hVal
+print("Graph added.")
 
-# G.nodes['x']['gVal'] = f(g)
-# G.nodes['x']['hVal'] = f(h)
-# G.nodes['x']['fVal'] = G.nodes['x']['gVal'] + G.nodes['x']['hVal']
-
-
-def sortLowF(list1):
-    list1.sort(key=lambda x:G.nodes[x]['fVal']) #sorting lowest fValue to be first
-    return list1
-
-
+##start
+def sortLowF(graph, list1):
+	list1.sort(key=lambda x:graph.nodes[x]['fVal'])
+	return list1
 
 def AStar(graph, start, end):
-    openlist = [start]
-    closedlist = []
-    path = []
-    curNode = start
-    
-    while True:
-      if not graph.has_node(start) or not graph.has_node(end):
-        print("Invalid nodes entered.")
-        return -1
-        break
-      if len(openlist) == 0:
-        print("No paths found.")
-        return -1
-        break
-      if curNode == end:
-        print("Shortest Path Found!")
-        return path
-        break
+	openlist = [start]
+	closedlist =[]
+	path = []
+	curNode = start
+	runcounter = 0
 
-      if curNode == start:
-        graph.nodes[curNode]['gVal'] = 0 
-        graph.nodes[curNode]['hVal'] = 0 
-        graph.nodes[curNode]['fVal'] = 0 
+	while True:
+		
+		curNode = sortLowF(graph, openlist)[0]
+		print("curNode: ", curNode)
 
-      else:
-        for x in openlist:
-          graph.nodes[x]['gVal'] = graph.nodes[curNode]['gVal'] + graph.edges[curNode,x]['weight']
-          graph.nodes[x]['hVal'] = 0 #enter direct distance function, should return float
-          graph.nodes[x]['fVal'] = graph.nodes[curNode]['gVal'] + graph.nodes[curNode]['hVal']
-    
-      #doesnt cater for 2 same lowest fVal
-      for n in openlist:
-        if n not in closedlist:
-          curNode = sortLowF(openlist)[0] # first element lowest f
-          closedlist.extend(openlist)
-          openlist = list(graph.neighbors(curNode)) #neighbors of curNode
-          path.append(curNode)
-          ##continue here****************************************************************
+		#breakconditions
+		if not graph.has_node(start) or not graph.has_node(end):
+			print("Invalid nodes entered.")
+			return -1
+			break
+		if len(openlist) == 0:
+			print("No paths found.")
+			return -1
+			break
+		if curNode == end:
+			print("Shortest Path Found!")
+			path.append(curNode)
+			return path
+			break
+
+		
+
+		if curNode == start:
+			graph.nodes[curNode]['gVal'] = 0 
+			graph.nodes[curNode]['hVal'] = 0 
+			graph.nodes[curNode]['fVal'] = 0
+
+			
+			closedlist.append(curNode)
+			openlist = list(graph.neighbors(curNode))
+			for x in openlist:
+				graph.nodes[x]['gVal'] = graph.nodes[curNode]['gVal'] + graph.edges[curNode,x]['weight']
+				#graph.nodes[x]['hVal'] = 0 #enter direct distance function, should return float TO 2DP
+				graph.nodes[x]['fVal'] = graph.nodes[x]['gVal'] + graph.nodes[x]['hVal']
+			openlist = sortLowF(graph, openlist)
+			path.append(curNode)
 
 
 
-startNode = 'a' #input("Enter start point by ID reference: ")
-endNode = 'e' #input("Enter end point by ID reference: ")
+		else:
+			closedlist.append(curNode)
+			openlist = sortLowF(graph, list(graph.neighbors(curNode)))
+			path.append(curNode)
+
+			for x in openlist:
+				if x in closedlist:
+					openlist.remove(x)
+				else:
+					graph.nodes[x]['gVal'] = graph.nodes[curNode]['gVal'] + graph.edges[curNode,x]['weight']
+					#graph.nodes[x]['hVal'] = 0 #enter direct distance function, should return float TO 2DP
+					graph.nodes[x]['fVal'] = graph.nodes[x]['gVal'] + graph.nodes[x]['hVal']
+					
+	return path
+
+
+startNode = 'a'
+endNode = 'e'
 print(AStar(G, startNode, endNode))
-
-#openlist = list(G.nodes)
-#print(sortLowF(openlist)[0])
-
-
-# a = G.nodes['a']['gVal']
-# print(a)
-# print(G.edges['a','c']['weight'])  
+#print(list(G.neighbors('d')))
