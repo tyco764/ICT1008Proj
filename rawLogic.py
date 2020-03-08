b788f6f4d23ba8c76c5bbbf6a17b62bdba259948
cleavengyj <60376701+cleavengyj@users.noreply.github.com>
2020-03-09 00:19:21 +0800

    Update rawLogic.py
    fixed bug
    might be working
    my bad @TarYen

--- rawLogic.py
+++ rawLogic.py
@@ -3,31 +3,10 @@ import time
 import matplotlib.pyplot as plt
 import networkx as nx
 from operator import itemgetter
-
-'''
-G = nx.Graph()
-
-G.add_edge('a', 'b', weight= 60) #weight = road dist correct to 2dp.
-G.add_edge('a', 'c', weight= 17)
-G.add_edge('c', 'd', weight= 10)
-G.add_edge('c', 'e', weight= 70)
-G.add_edge('c', 'f', weight= 90)
-G.add_edge('a', 'd', weight= 30)
-G.add_edge('d', 'e', weight= 30)
-
-G.add_node('a', gVal=0, hVal=30, fVal=0)
-G.add_node('b', gVal=0, hVal=100, fVal=0)
-G.add_node('c', gVal=0, hVal=50, fVal=0)
-G.add_node('d', gVal=0, hVal=20, fVal=0)
-G.add_node('e', gVal=0, hVal=0, fVal=0)
-G.add_node('f', gVal=0, hVal=120, fVal=0)
-'''
-# function to find hVal (direct dist) from one node straight to end node
+# to find hVal (direct dist) from one node straight to end node
 # function to find gVal (collective dist) distance of path
 
 #print("Graph added.")
-
-
 ##start
 
 def addedges(df):
@@ -35,11 +14,6 @@ def addedges(df):
 
 
 def sortLowF(graph, list1, ):
-	# x = graph.nodes
-	# list1.sort(key=lambda x:graph.nodes["fVal"])
-	# print(list1)
-
-	#print(graph.nodes['289A'])
 	list1.sort(key= lambda x : graph.nodes[x]['fVal'])
 	print("list1: ", list1)
 	return list1
@@ -49,13 +23,11 @@ def AStar(graph, start, end):
 	openlist = [start]
 	closedlist = []
 	path = []
-	curNode = start
 	runcounter = 0
 	tim = time.time()
 	print("0")
 
 	while True:
-
 		curNode = sortLowF(graph, openlist)[0]
 		print("curNode: ", curNode)
 
@@ -82,11 +54,9 @@ def AStar(graph, start, end):
 			closedlist.append(curNode)
 			openlist = list(graph.neighbors(curNode))
 			for x in openlist:
-
 				graph.nodes[x]['gVal'] = graph.nodes[curNode]['gVal'] + graph.edges[curNode, x]['weight']
-				# graph.nodes[x]['hVal'] = 0 #enter direct distance function, should return float TO 2DP
+				# graph.nodes[x]['hVal'] = 0 #enter direct distance function, should return float
 				graph.nodes[x]['fVal'] = graph.nodes[x]['gVal'] + graph.nodes[x]['hVal']
-
 			openlist = sortLowF(graph, openlist)
 			path.append(curNode)
 
@@ -94,21 +64,13 @@ def AStar(graph, start, end):
 			closedlist.append(curNode)
 			openlist = sortLowF(graph, list(graph.neighbors(curNode)))
 			path.append(curNode)
-
+			openlist = [x for x in openlist if x not in closedlist]
 			for x in openlist:
-				if x in closedlist:
-					openlist.remove(x)
-				else:
-
-					graph.nodes[x]['gVal'] = graph.nodes[curNode]['gVal'] + graph.edges[curNode, x]['weight']
-					# graph.nodes[x]['hVal'] = 0 #enter direct distance function, should return float TO 2DP
-					graph.nodes[x]['fVal'] = graph.nodes[x]['gVal'] + graph.nodes[x]['hVal']
+				graph.nodes[x]['gVal'] = graph.nodes[curNode]['gVal'] + graph.edges[curNode, x]['weight']
+				# graph.nodes[x]['hVal'] = 0 #enter direct distance function, should return float TO 2DP
+				graph.nodes[x]['fVal'] = graph.nodes[x]['gVal'] + graph.nodes[x]['hVal']
 
 		runcounter += 1
 		if runcounter % 10000 == 0: print(time.time() - tim, path)
 	return path
 
-# startNode = 'a'
-# endNode = 'e'
-# print(AStar(G, startNode, endNode))
-# print(list(G.neighbors('d')))
