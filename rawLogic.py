9155b4a84f8f80a03d0ce596eeec8781b77cf7cb
cleavengyj <60376701+cleavengyj@users.noreply.github.com>
2020-03-10 22:24:04 +0800

    Update rawLogic.py
    clean up, fixed backtrack not 100% sure working, need help with testing.
    heuristics still haywire

--- rawLogic.py
+++ rawLogic.py
@@ -3,14 +3,12 @@ import time
 import matplotlib.pyplot as plt
 import networkx as nx
 from operator import itemgetter
-# to find hVal (direct dist) from one node straight to end node
-# function to find gVal (collective dist) distance of path
 
-#print("Graph added.")
-##start
 
-def addedges(df):
-	pass
+def backtrack(graph, curNode, path, closedlist):
+    path.remove(curNode)
+    curNode = path[len(path)-1]
+    openlist = [x for x in list(graph.neighbors(curNode)) if x not in closedlist]
 
 
 def sortLowF(graph, list1, ):
@@ -20,69 +18,44 @@ def sortLowF(graph, list1, ):
 
 
 def AStar(graph, start, end):
+	if not graph.has_node(start) or not graph.has_node(end):
+	        return "Invalid Input.\n"
+
 	openlist = [start]
 	closedlist = []
 	path = []
 	runcounter = 0
 	tim = time.time()
-	print("0")
-	curNode = None
-	#hardcoded(debug purpose)
+	print("0")    
+	graph.nodes[start]['fVal'] =0
+	graph.nodes[start]['gVal'] =0
+	graph.nodes[start]['hVal'] =0
 	graph.nodes[end]['hVal'] = 0
 	
 	while True:
 		if len(openlist) == 0:
-			print("No paths found.")
-			path.remove(curNode)
-			closedlist.remove(curNode)
-			tempnode = curNode
-			curNode = path[len(path)-1]
-			openlist = sortLowF(graph, list(graph.neighbors(curNode)))
-			openlist.remove(tempnode)
+            		print("No paths found.")
+            		backtrack(graph, curNode, path, closedlist)
+		try:
+            		curNode = sortLowF(graph,openlist)[0]
+		except IndexError:
+            		curNode = path[len(path)-1]
+            		backtrack(graph,curNode,path, closedlist)
 
-
-		curNode = sortLowF(graph, openlist)[0]
-		print("curNode: ", curNode)
-
-		# breakconditions
-		if not graph.has_node(start) or not graph.has_node(end):
-			print("Invalid nodes entered.")
-			return -1
-
-
-		if curNode == end:
-			print("Shortest Path Found!")
-			path.append(curNode)
-			return path
-
-		# to find hVal (direct dist) from one node straight to end node
-		# function to find gVal (collective dist) distance of path
-		if curNode == start:
-			graph.nodes[curNode]['gVal'] = 0
-			graph.nodes[curNode]['hVal'] = 0
-			graph.nodes[curNode]['fVal'] = 0
-
-			closedlist.append(curNode)
-			openlist = list(graph.neighbors(curNode))
-			for x in openlist:
-				graph.nodes[x]['gVal'] = graph.nodes[curNode]['gVal'] + graph.edges[curNode, x]['weight']
-				# graph.nodes[x]['hVal'] = 0 #enter direct distance function, should return float
-				graph.nodes[x]['fVal'] = graph.nodes[x]['gVal'] + graph.nodes[x]['hVal']
-			openlist = sortLowF(graph, openlist)
-			path.append(curNode)
-
-		else:
-			closedlist.append(curNode)
-			openlist = sortLowF(graph, list(graph.neighbors(curNode)))
-			path.append(curNode)
-			openlist = [x for x in openlist if x not in closedlist]
-			for x in openlist:
-				graph.nodes[x]['gVal'] = graph.nodes[curNode]['gVal'] + graph.edges[curNode, x]['weight']
-				# graph.nodes[x]['hVal'] = 0 #enter direct distance function, should return float TO 2DP
+        	openlist = [x for x in list(graph.neighbors(curNode)) if x not in closedlist]
+        	path.append(curNode)
+        	for x in openlist:
+			if x is end:
+				path.append(x)
+				print ("Path found!")
+				return path
+			else:
+				graph.nodes[x]['gVal'] = graph.nodes[curNode]['gVal'] + graph.edges[curNode,x]['weight']
+				#graph.nodes[x]['hVal'] = getEucDist(end, x)
 				graph.nodes[x]['fVal'] = graph.nodes[x]['gVal'] + graph.nodes[x]['hVal']
 
-		runcounter += 1
-		if runcounter % 10000 == 0: print(time.time() - tim, path)
-
-	#return path
+		closedlist.append(curNode)
+	return "No path found"
+	runcounter += 1
+	if runcounter % 10000 == 0: print(time.time() - tim, path)
 
