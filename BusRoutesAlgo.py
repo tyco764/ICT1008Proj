b36c2e28284e0f6c89ada16b26e318c1627793d3
tyco <tykoh764@gmail.com>
2020-03-14 19:55:55 +0800

    Slightly Change format of BusAlgo

--- BusRoutesAlgo.py
+++ BusRoutesAlgo.py
@@ -7,16 +7,10 @@ import csv
 #print("Enter end point: ")
 #end_point = input()
 
-start_point = '65009'
-end_point = '65169'
-
-least_stops = 32
-
-with open('BusRoutes.csv', mode = 'r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ',')
-    
-    for row in csv_reader:
-        #print(row)
+def BusAlgo(csvdata):
+    least_stops = 32
+    for row in csvdata:
+        # print(row)
         if start_point in row and end_point in row:
             least_stops_temp = row.index(end_point) - row.index(start_point)
             if least_stops_temp > 0:
@@ -25,8 +19,22 @@ with open('BusRoutes.csv', mode = 'r') as csv_file:
                     least_stops_print = (str(least_stops) + " stop(s) with Bus Service " + row[0])
                 elif least_stops_temp == least_stops:
                     least_stops_print += ", " + row[0]
-                    
+
     if (least_stops != 32):
-        print(least_stops_print)
+        return least_stops_print
+        #print(least_stops_print)
     else:
-        print("There are no direct buses.")
+        return "There are no direct buses."
+        #print("There are no direct buses.")
+
+start_point = '65009'
+end_point = '65169'
+
+least_stops = 32
+least_stops_print = ""
+with open('csv/BusRoutes.csv', mode = 'r') as csv_file:
+    csvdata = csv.reader(csv_file, delimiter = ',')
+    least_stops_print = BusAlgo(csvdata)
+
+print(least_stops_print)
+
